b4341dbed41cb9c4c3c6f3fbf09bbec3eefbc2bd
tao tang <rache008@163.com>
2018-05-15 10:21:35 +0800

    组装完成 一个简单的文本过滤工具

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-#name=find_select_in_code 
+#name=find_select_in_code
 #author=lhtangtao
 #time=2018/5/14 20:03
 #Mail=tangtao@lhtangtao.com
@@ -9,4 +9,23 @@
 #my_website=http://www.lhtangtao.com
 #Description=添加描述信息
 """
+from file import get_all_file, find_keyword_in_file
+from folder import get_folder_useful
 
+
+def final(kind, path, keyword):
+    file_list = []
+    folder_list = []
+    folder = get_folder_useful(path)  # 获取所有的有用文件夹 返回一个列表
+    for i in folder:
+        folder_list.append(i)
+    for i in folder_list:
+        folder_file = get_all_file(i, kind)  # 文件夹里的文件
+        for i in folder_file:
+            file_list.append(i)
+    for i in file_list:
+        find_keyword_in_file(i, keyword)
+
+
+if __name__ == '__main__':
+    final(".java", "/Users/taotang/GitOfGeely/judger/", "select")
